8c3c11fb39dc9f5b741d51bc5f5d74fa9a671483
erfontanini <erfontanini@gmail.com>
2023-04-18 09:55:22 -0300

    primer commit

--- abc.py
+++ abc.py
@@ -5,3 +5,4 @@ paginas_leidas = input("escribe la cantidad de paginas leidas")
 paginas_restantes = int(paginas) - int(paginas_leidas)
 print("Te faltas para terminar de leer" + paginas_restantes + "páginas")
 
+print("Hola")
