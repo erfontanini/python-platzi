790ca3fa38df03be11e1444dba2394b120e97bd9
erfontanini <erfontanini@gmail.com>
2023-04-18 11:20:55 -0300

    .

--- app/charts.py
+++ app/charts.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
+  plt.savefig("bar.png")
   plt.show()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
+  plt.savefig("pie.png")
   plt.show()
 
 if __name__ == '__main__':
